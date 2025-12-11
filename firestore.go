32649eafc25e632afa9f2aafd90d718dcad5951a
Colin <colin1124x@gmail.com>
2025-12-11 22:10:56 +0800

    .

--- firestore.go
+++ firestore.go
@@ -232,6 +232,8 @@ func SaveCurrentData(d *Data) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	d.LastUpdateTime = time.Now()
+
 	_, err = client.Collection(FirestoreCollection).
 		Doc(FirestoreDocID).
 		Set(ctx, d.Map())
